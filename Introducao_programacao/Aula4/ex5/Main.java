7a050429f24ec370c8a8d13326858e3d400b49c2
LuccaPCDoria <doria.lucca@gmail.com>
2026-03-13 18:20:27 -0300

    Subindo att no aula04 ex05

--- Introducao_programacao/Aula4/ex5/Main.java
+++ Introducao_programacao/Aula4/ex5/Main.java
@@ -6,18 +6,52 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc1 = new Scanner(System.in);
         Scanner sc2 = new Scanner(System.in);
+
         Produto p = new Produto();
 
-        System.out.print("Digite o código do produto: ");
-        p.setNome(sc1.nextLine());
+        System.out.println("Seja bem vindo a loja do Lucca: ");
+        System.out.println("Se deseja cadastrar um produto digite (1):\nSe deseja comprar um produto digite (2):\nSe deseja sair digite (3):");
+        int opt = sc2.nextInt();
+        while(opt != 3){
+            System.out.println("Se deseja cadastrar um produto digite (1):\nSe deseja comprar um produto digite (2):\nSe deseja sair digite (3):");
+            opt = sc2.nextInt();
+            if(opt == 1){
+                System.out.print("Digite o código do produto: ");
+                p.setCodigo(sc2.nextInt());
 
-        System.out.print("Digite sua altura: ");
-        p.setAltura(sc2.nextDouble());
+                System.out.print("Digite o nome do produto: ");
+                p.setNome(sc1.nextLine());
 
-        System.out.print("Digite seu peso: ");
-        p.setPeso(sc2.nextDouble());
-        p.calcula_imc();
-        System.out.printf("O indice de massa corporal de %s é %.2f", p.getNome(), p.getIndiceMC());
+                System.out.print("Digite o tamanho do produto: ");
+                p.setTamanho(sc2.nextDouble());
+
+                System.out.print("Digite o peso do produto: ");
+                p.setPeso(sc2.nextDouble());
+
+                System.out.print("Digite a cor do produto: ");
+                p.setCor(sc1.nextLine());
+
+                System.out.print("Digite o valor do produto: ");
+                p.setValor(sc2.nextDouble());
+
+                System.out.print("Digite a quantidade em estoque do produto: ");
+                p.setQuant_estoque(sc2.nextInt());
+            }
+            else if(opt == 2){
+                System.out.println("Qual a forma de pagamento: \n1 -> debito\n2 -> pix\n3 -> dinheiro\n4 -> crédito");
+                int fm = sc2.nextInt();
+                System.out.println("Qual é o valor: ");
+                double valor = sc2.nextDouble();
+                p.formato_pagamento(fm, valor);
+            }
+            else{
+                System.out.println("Opção invalida! Tente novamente.");
+            }
+        }
+        System.out.println("Saiu do sistema");
+
+
+
 
         sc1.close();
         sc2.close();
--- Introducao_programacao/Aula4/ex5/Produto.java
+++ Introducao_programacao/Aula4/ex5/Produto.java
@@ -1,12 +1,33 @@
 package Aula4.ex5;
 
 public class Produto {
+
     private int codigo, quant_estoque;
     private String nome, cor;
-    private double peso, altura, indiceMC, valor;
+    private double peso, valor, tamanho;
 
-    public void calcula_imc() {
-        indiceMC = peso / (altura * altura);
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public double getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(double tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getQuant_estoque() {
+        return quant_estoque;
+    }
+
+    public void setQuant_estoque(int quant_estoque) {
+        this.quant_estoque = quant_estoque;
     }
 
     public String getNome() {
@@ -17,15 +38,59 @@ public class Produto {
         this.nome = nome;
     }
 
-    public double getIndiceMC() {
-        return indiceMC;
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public double getPeso() {
+        return peso;
     }
 
     public void setPeso(double peso) {
         this.peso = peso;
     }
 
-    public void setAltura(double altura) {
-        this.altura = altura;
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String formato_pagamento(int forma_pagar, double valor){
+        if(forma_pagar == 1) return debito(valor);
+        else if (forma_pagar == 2) return pix(valor);
+        else if (forma_pagar == 3) return dinheiro(valor);
+        else if(forma_pagar == 4) return credito(valor);
+        else return "Forma de pagamento invalida!";
+    }
+    public String debito(double valor) {
+        System.out.println("A opção deseja foi debito e você recebeu 5% de desconto");
+        if (valor > this.valor) return "Pagamento realizado com sucesso";
+        else return "Saldo não suficiente";
+    }
+    public String pix(double valor) {
+        System.out.println("A opção deseja foi pix e você recebeu 5% de desconto");
+        if (valor > this.valor) return "Pagamento realizado com sucesso!";
+        else return "Saldo não suficiente";
+    }
+    public String dinheiro(double valor) {
+        System.out.println("A opção deseja foi dinheiro e você recebeu 5% de desconto.");
+        double troco = 0;
+        if (valor > this.valor) {
+            troco = valor - (this.valor - (this.valor * 0.05));
+            return "Pagamento realizado com sucesso!\nSeu troco é: R$" + troco;
+        }else return "Saldo não suficiente";
+    }
+    public String credito(double valor) {
+        System.out.println("A opção deseja foi credito e você pode parcelar até 3x sem juros.");
+        double total = this.valor / 3;
+        if (valor > total) return "Pagamento realizado com sucesso!";
+        else return "Saldo não suficiente";
     }
 }
